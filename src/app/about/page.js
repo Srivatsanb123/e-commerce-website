79ecb84eec73406e1aff05529515609e7200b677
Srivatsanb123 <srivatsanb123@gmail.com>
2023-09-09 23:12:44 +0530

    Changes made

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -16,7 +16,7 @@ function AboutPage() {
       <div className="container mx-auto mt-4">
         <h2 className="text-2xl font-semibold mb-4">About Us</h2>
         <p>
-          Welcome to our online electronics store! We are passionate about providing you with a wide range of high-quality electronic products at competitive prices. Our goal is to make technology accessible to everyone, whether you're a seasoned tech enthusiast or a casual user looking for reliable gadgets.
+          Welcome to our online electronics store! We are passionate about providing you with a wide range of high-quality electronic products at competitive prices. Our goal is to make technology accessible to everyone, whether you&apos;re a seasoned tech enthusiast or a casual user looking for reliable gadgets.
         </p>
         <p>
           At E-commerce Website, we carefully curate our selection of products to ensure they meet the highest standards of quality and performance. Our team of experts stays up-to-date with the latest trends and innovations in the electronics industry to bring you the most cutting-edge products.
